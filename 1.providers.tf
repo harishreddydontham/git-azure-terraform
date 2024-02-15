6e01c231bcd3160d0de4acabbba273df24226cf7
harishreddydontham <harishreddydontham@gmail.com>
2024-02-15 10:47:30 -0600

    added passwords

--- 1.providers.tf
+++ 1.providers.tf
@@ -3,7 +3,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "<=3.91.0"
-     
+
     }
 
     aws = {
---
+++ 3.random-passwords.tf
@@ -0,0 +1,10 @@
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  min_lower        = 4
+  min_upper        = 4
+  min_special      = 4
+  min_numeric     = 4
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
